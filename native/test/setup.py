2d759a51c03df90ee040474e05c6ffa87c8eb865
attilagombos <attilagombos@syntaxsugar.net>
2024-03-22 16:03:32 +0000

    wip

---
+++ native/test/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='test-module',
+    version='1.0.0',
+    description='Test module for packaging',
+    author='Global Scope Ltd.',
+    author_email='info@globalscope.info',
+    packages=['main']
+)
